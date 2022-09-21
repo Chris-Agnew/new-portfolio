0b157a2ccdb8e0f35a6b8be9559ac0b40492c4df
Chris Agnew <chris@chris-agnew.com>
2022-09-21 08:28:06 -0400

    add experience section

---
+++ components/ExperienceCard.tsx
@@ -0,0 +1,62 @@
+import { motion } from "framer-motion";
+import Image from "next/image";
+import { urlFor } from "../sanity";
+import { Experience } from "../typings";
+
+type Props = {
+  exp: Experience;
+};
+
+const ExperienceCard = ({ exp }: Props) => {
+  return (
+    <article className="flex flex-col rounded-lg items-center space-y-7 flex-shrink-0 w-[500px] md:w-[600px] xl:w-[900px] bg-[#292929] p-10 opacity-40 hover:opacity-100 cursor-pointer transition-opacity duration-200 overflow-hidden">
+      <motion.img
+        initial={{
+          opacity: 0,
+          y: -100,
+        }}
+        transition={{
+          duration: 1.2,
+        }}
+        whileInView={{
+          opacity: 1,
+          y: 0,
+        }}
+        viewport={{ once: true }}
+        src="/images/chris-profile.jpg"
+        alt=""
+        className="w-32 h-32 rounded-full xl:w-[200px] xl:h-[200px] object-cover object-center"
+      />
+      <div className="px-0 md:px-10">
+        <h4 className="text-4xl font-light">{exp.jobTitle}</h4>
+        <p className="font-bold text-2xl mt-1">{exp.company}</p>
+        <div className="flex space-x-2 my-2">
+          {exp.technologies.map((tech: any) => (
+            <Image
+              key={tech._id}
+              src={urlFor(tech.image).url()}
+              width={50}
+              height={50}
+              alt={tech.title}
+              className=" object-cover object-center px-3"
+            />
+          ))}
+        </div>
+        <p className="uppercase py-5 text-gray-300">
+          {new Date(exp.dateStarted).toLocaleDateString()} -
+          {exp.isCurrentlyWorkingHere
+            ? "Present"
+            : new Date(exp.dateEnded).toLocaleDateString()}
+        </p>
+        <ul className="list-disc space-y4 ml-5 text-lg">
+          {exp.points &&
+            exp.points.map((point: string, i: number) => (
+              <li key={i}>{point}</li>
+            ))}
+        </ul>
+      </div>
+    </article>
+  );
+};
+
+export default ExperienceCard;
---
+++ components/WorkExperience.tsx
@@ -0,0 +1,31 @@
+import { motion } from "framer-motion";
+import { Experience } from "../typings";
+import ExperienceCard from "./ExperienceCard";
+
+type Props = {
+  experience: Experience[];
+};
+
+const WorkExperience = ({ experience }: Props) => {
+  return (
+    <motion.div
+      className="h-screen flex relative overflow-hidden flex-col text-left md:flex-row max-w-full px-10 justify-evenly mx-auto items-center"
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1.5 }}
+    >
+      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
+        Experience
+      </h3>
+      <div className="w-full flex space-x-5 overflow-x-scroll p-10 snap-x snap-mandatory scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80">
+        {experience
+          ?.sort((a: any, b: any) => b.dateEnded - a.dateEnded)
+          .map((experience) => (
+            <ExperienceCard key={experience._id} exp={experience} />
+          ))}
+      </div>
+    </motion.div>
+  );
+};
+
+export default WorkExperience;
