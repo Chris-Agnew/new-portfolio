aaf9c931e557eeb3b5486332ddce15a78d42ec3f
Chris Agnew <hello@chrisagnew.codes>
2024-07-27 21:51:06 -0400

    refactor: add accessibility

--- components/About.tsx
+++ components/About.tsx
@@ -11,9 +11,14 @@ const About = ({ pageInfo }: Props) => {
       initial={{ opacity: 0 }}
       whileInView={{ opacity: 1 }}
       transition={{ duration: 1.5 }}
-      className="flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl  mx-auto px-10  justify-evenly items-center"
+      className="flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl mx-auto px-10 justify-evenly items-center"
+      role="region"
+      aria-labelledby="about-heading"
     >
-      <h3 className="absolute top-24 hidden md:flex uppercase tracking-[20px] text-gray-500 text-2xl">
+      <h3
+        id="about-heading"
+        className="absolute top-24 hidden md:flex uppercase tracking-[20px] text-gray-500 text-2xl"
+      >
         About
       </h3>
 
@@ -32,10 +37,11 @@ const About = ({ pageInfo }: Props) => {
         }}
         viewport={{ once: true }}
         className="-mb-20 md:mb-0 flex-shrink-0 w-56 h-56 object-cover rounded-full md:rounded-lg md:w-64 md:h-95 xl:w-[500px] xl:h-[500px] hidden md:flex"
+        alt="Chris Agnew at  Advanced RV"
       />
       <div className="space-y-10 px-0 md:px-10">
-        <h4 className="text:lg md:text-4xl font-semibold">
-          Here is a{" "}
+        <h4 className="text-lg md:text-4xl font-semibold">
+          Here is a
           <span className="underline decoration-[#F7AB0A]/50">little</span>{" "}
           background
         </h4>
