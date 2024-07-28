4516c85753d1ad563b113c0602682d1122c0f5d8
Chris Agnew <hello@chrisagnew.codes>
2024-07-27 21:43:41 -0400

    refactor: add accessibility to Footer Component

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,14 +4,18 @@ import Link from "next/link";
 const Footer = () => {
   return (
     <Link href="#hero">
-      <footer className="sticky bottom-5  w-full cursor-pointer">
+      <footer
+        className="sticky bottom-5 w-full cursor-pointer"
+        role="contentinfo"
+      >
         <div className="flex justify-center items-center">
           <Image
             src="/images/chris-agnew-logo.png"
             height={50}
             width={50}
-            alt="chris agnew logo"
+            alt="Chris Agnew logo"
             className="rounded-full filter grayscale hover:grayscale-0 cursor-pointer object-cover"
+            aria-label="Back to top"
           />
         </div>
       </footer>
