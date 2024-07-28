645f700d258acd42a8cb9fc678bea0ac70c22ec8
Chris Agnew <hello@chrisagnew.codes>
2024-07-27 22:04:04 -0400

    refactor(accessibility): add accessibility

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import type { GetStaticProps, NextPage } from "next";
+import type { GetStaticProps } from "next";
 import Head from "next/head";
 import About from "../components/About";
 import Contact from "../components/Contact";
@@ -8,7 +8,7 @@ import Hero from "../components/Hero";
 import Projects from "../components/Projects";
 import WorkExperience from "../components/WorkExperience";
 import Seo from "../components/Seo";
-import { Experience, PageInfo, Project, Skill, Social } from "../typings";
+import { Experience, PageInfo, Project, Social } from "../typings";
 import { sanityClient } from "../sanity";
 import { groq } from "next-sanity";
 
@@ -16,42 +16,48 @@ type Props = {
   pageInfo: PageInfo;
   experience: Experience[];
   projects: Project[];
-  skills: Skill[];
   socials: Social[];
 };
 
-const Home = ({ pageInfo, experience, projects, skills, socials }: Props) => {
+const Home = ({ pageInfo, experience, projects, socials }: Props) => {
   return (
-    <div className="bg-[rgb(36,36,36)] text-white h-screen  overflow-scroll scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80 font-nunito-sans">
-      <Seo
-        title="Chris Agnew | Next.js and Tailwind CSS Developer"
-        description="Chris Agnew is a Full-Stack Developer based out of Cleveland, Ohio. He specializes in making sites and apps with Next.js and Tailwind CSS"
-      />
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="bg-[rgb(36,36,36)] text-white h-screen overflow-scroll scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80 font-nunito-sans">
+        <Seo
+          title="Chris Agnew | Next.js and Tailwind CSS Developer"
+          description="Chris Agnew is a Full-Stack Developer based out of Cleveland, Ohio. He specializes in making sites and apps with Next.js and Tailwind CSS"
+        />
 
-      <Header socials={socials} />
+        <Header socials={socials} />
 
-      <section id="hero">
-        <Hero pageInfo={pageInfo} />
-      </section>
+        <main>
+          <section id="hero" aria-labelledby="hero-section">
+            <Hero pageInfo={pageInfo} />
+          </section>
 
-      <section id="about">
-        <About pageInfo={pageInfo} />
-      </section>
+          <section id="about" aria-labelledby="about-section">
+            <About pageInfo={pageInfo} />
+          </section>
 
-      <section id="experience">
-        <WorkExperience experience={experience} />
-      </section>
+          <section id="experience" aria-labelledby="experience-section">
+            <WorkExperience experience={experience} />
+          </section>
 
-      <section id="projects">
-        <Projects projects={projects} />
-      </section>
+          <section id="projects" aria-labelledby="projects-section">
+            <Projects projects={projects} />
+          </section>
 
-      <section id="contact">
-        <Contact />
-      </section>
+          <section id="contact" aria-labelledby="contact-section">
+            <Contact />
+          </section>
+        </main>
 
-      <Footer />
-    </div>
+        <Footer />
+      </div>
+    </>
   );
 };
 
@@ -68,20 +74,17 @@ export const getStaticProps: GetStaticProps<Props> = async () => {
   ...,
   technologies[]->
 }`;
-  const skillsQuery = groq`*[_type == "skill"]`;
   const socialsQuery = groq`*[_type == "social"]`;
 
   const pageInfo: PageInfo = await sanityClient.fetch(pageInfoQuery);
   const experience: Experience[] = await sanityClient.fetch(experienceQuery);
   const projects: Project[] = await sanityClient.fetch(projectsQuery);
-  const skills: Skill[] = await sanityClient.fetch(skillsQuery);
   const socials: Social[] = await sanityClient.fetch(socialsQuery);
   return {
     props: {
       pageInfo,
       experience,
       projects,
-      skills,
       socials,
     },
     revalidate: 60,
