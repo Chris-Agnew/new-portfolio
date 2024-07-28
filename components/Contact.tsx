a6c318e183e4746033419a7e54ff54011f016ef3
Chris Agnew <hello@chrisagnew.codes>
2024-07-27 21:47:57 -0400

    refactor: add accessiblity and remove calendly

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,29 +1,41 @@
 import { MapPinIcon, EnvelopeIcon } from "@heroicons/react/24/solid";
-import Calendly from "./Calendly";
-type Props = {};
 
-const Contact = (props: Props) => {
+const Contact = () => {
   return (
-    <div className="relative h-screen flex flex-col text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center">
+    <div
+      className="relative h-screen flex flex-col text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center"
+      role="form"
+      aria-labelledby="contact-heading"
+    >
       <div className="flex flex-col space-y-10">
-        <h4 className="text-4xl font-semibold text-center">
+        <h4 id="contact-heading" className="text-4xl font-semibold text-center">
           Have a project you&apos;d like to discuss?
           <span className="decoration-[#F7AB0A]/50 underline px-2">
-            Lets Talk.
+            Let&apos;s Talk.
           </span>
         </h4>
         <div className="space-y-10">
           <div className="flex justify-center items-center space-x-5">
-            <MapPinIcon className="text-[#F7Ab0A] h-7 w-7 animate-pulse" />
-            <p className="text-2xl">Fredericksburg, VA</p>
+            <MapPinIcon
+              className="text-[#F7Ab0A] h-7 w-7 animate-pulse"
+              aria-hidden="true"
+            />
+            <p className="text-2xl">Conneaut, Ohio</p>
           </div>
           <div className="flex justify-center items-center space-x-5">
-            <EnvelopeIcon className="text-[#F7Ab0A] h-7 w-7 animate-pulse" />
+            <EnvelopeIcon
+              className="text-[#F7Ab0A] h-7 w-7 animate-pulse"
+              aria-hidden="true"
+            />
             <p className="text-2xl">
-              <a href="mailto:hello@chrisagnew.codes">hello@chrisagnew.codes</a>{" "}
+              <a
+                href="mailto:hello@chrisagnew.codes"
+                aria-label="Email Chris Agnew"
+              >
+                hello@chrisagnew.codes
+              </a>
             </p>
           </div>
-          <Calendly url="https://calendly.com/chris-agnew-1/30min" />
         </div>
       </div>
     </div>
