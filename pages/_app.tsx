4cf0b136ff0b5e81595d07d701914167662494bc
Chris Agnew <hello@chrisagnew.codes>
2024-07-29 07:18:08 -0400

    feat(analytics): add google tag manager

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import { GoogleTagManager } from "@next/third-parties/google";
+
+import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <GoogleTagManager gtmId="GTM-M9J75R27" />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
