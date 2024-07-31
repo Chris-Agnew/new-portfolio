c12fe810a0ee819e2776eb51ae553d58e11a106b
Chris Agnew <hello@chrisagnew.codes>
2024-07-30 21:28:57 -0400

    feat(vercel): add vercel speed insights

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,14 @@
 import "../styles/globals.css";
 import { GoogleTagManager } from "@next/third-parties/google";
-
 import type { AppProps } from "next/app";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Component {...pageProps} />
       <GoogleTagManager gtmId="GTM-M9J75R27" />
+      <SpeedInsights />
     </>
   );
 }
