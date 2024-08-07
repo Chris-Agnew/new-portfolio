97c1407fc631b9a03b084ffc095108ce28e8fbff
Chris Agnew <hello@chrisagnew.codes>
2024-08-06 20:59:02 -0400

    feat(cv): add download button for CV

--- components/About.tsx
+++ components/About.tsx
@@ -1,5 +1,6 @@
 import { motion } from "framer-motion";
 import { PageInfo } from "../typings";
+import { ResumeDownload } from "./ResumeDownload";
 
 type Props = {
   pageInfo: PageInfo;
@@ -7,41 +8,44 @@ type Props = {
 
 const About = ({ pageInfo }: Props) => {
   return (
-    <motion.div
-      initial={{ opacity: 0 }}
-      whileInView={{ opacity: 1 }}
-      transition={{ duration: 1.5 }}
-      className="flex flex-col relative mb-36 text-left md:flex-row max-w-7xl mx-auto px-10 justify-evenly items-center"
-      role="region"
-      aria-labelledby="about-heading"
-    >
-      <motion.img
-        src="/images/chris-agnew-headshot.webp"
-        initial={{
-          x: -200,
-          opacity: 0,
-        }}
-        whileInView={{
-          x: 0,
-          opacity: 1,
-        }}
-        transition={{
-          duration: 1.5,
-        }}
-        viewport={{ once: true }}
-        className="-mb-20 md:mb-0 flex-shrink-0 w-56 h-56 object-cover rounded-full md:rounded-lg md:w-64 md:h-95 xl:w-[500px] xl:h-[500px] hidden md:flex"
-        alt="Chris Agnew Headshot"
-        loading="lazy"
-      />
-      <div className="space-y-10 px-0 md:px-10">
-        <h3 className="text-lg md:text-4xl font-semibold">
-          Here is a{" "}
-          <span className="underline decoration-[#F7AB0A]/50">little</span>{" "}
-          background
-        </h3>
-        <p className="text-base">{pageInfo?.backgroundInformation}</p>
-      </div>
-    </motion.div>
+    <>
+      <motion.div
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 1.5 }}
+        className="flex flex-col relative mb-12 text-left md:flex-row max-w-7xl mx-auto px-10 justify-evenly items-center"
+        role="region"
+        aria-labelledby="about-heading"
+      >
+        <motion.img
+          src="/images/chris-agnew-headshot.webp"
+          initial={{
+            x: -200,
+            opacity: 0,
+          }}
+          whileInView={{
+            x: 0,
+            opacity: 1,
+          }}
+          transition={{
+            duration: 1.5,
+          }}
+          viewport={{ once: true }}
+          className="-mb-20 md:mb-0 flex-shrink-0 w-56 h-56 object-cover rounded-full md:rounded-lg md:w-64 md:h-95 xl:w-[500px] xl:h-[500px] hidden md:flex"
+          alt="Chris Agnew Headshot"
+          loading="lazy"
+        />
+        <div className="space-y-10 px-0 md:px-10">
+          <h3 className="text-lg md:text-4xl font-semibold">
+            Here is a{" "}
+            <span className="underline decoration-[#F7AB0A]/50">little</span>{" "}
+            background
+          </h3>
+          <p className="text-base ">{pageInfo?.backgroundInformation}</p>
+        </div>
+      </motion.div>
+      <ResumeDownload />
+    </>
   );
 };
 
---
+++ components/ResumeDownload.tsx
@@ -0,0 +1,21 @@
+import { motion } from "framer-motion";
+
+export const ResumeDownload: React.FC = () => {
+  return (
+    <motion.div
+      className="flex justify-center items-center mb-10"
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1.5 }}
+      aria-labelledby="Download CV PDF"
+    >
+      <a
+        href="/resume.pdf"
+        download="chris-agnew-fullstack-dev-resume"
+        className="p-3 bg-[#F7AB0A]/80 rounded-lg"
+      >
+        Download CV PDF
+      </a>
+    </motion.div>
+  );
+};
